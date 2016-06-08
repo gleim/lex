e1a7b58f3aed360ff4a1c569be492fa3d8120542
Bill Gleim <bgleim@gmail.com>
2016-06-07 21:39:40 -0700

    Upgrade to new ArtFactory design pattern

--- ArtFactory/meteor/app/client/af.js
+++ ArtFactory/meteor/app/client/af.js
@@ -31,9 +31,9 @@ Template['publications'].helpers({
 
 Template['view'].events({
     'click button.view': function (e, template) {
-        alert(template.find('input').value +' is attempting to view content.  \nPurchased = '+ LexContentInstance.purchased.call({from: template.find('input').value, gas: 50000}));
+        alert(template.find('input').value +' is attempting to view content.  \nPurchased = '+ ArtFactory.purchased.call({from: template.find('input').value, gas: 50000}));
 
-        if (LexContentInstance.purchased.call({from: template.find('input').value, gas: 50000}) == true)
+        if (ArtFactory.purchased.call({from: template.find('input').value, gas: 50000}) == true)
         // this will download the file in absence of an appropriate browser plugin
             window.open('http://localhost:8080/ipfs/QmTca4A43f4kEvzTouvYTegtp6KobixRqweV12NrvwwtFP');
 
@@ -43,7 +43,7 @@ Template['view'].events({
 
 Template['pay'].events({
     'click button.pay': function (e, template) {
-        alert(template.find('input').value +' is '+ LexContentInstance.view(template.find('input').value));
+        alert(template.find('input').value +' is '+ ArtFactory.view(template.find('input').value));
         template.find('input').value = '';
     },
 }); 
@@ -51,7 +51,7 @@ Template['pay'].events({
 Template['publish'].events({
     'click button.publish': function (e, template) {
         console.log("FORM INPUT: " + template.find('input').value);
-        LexContentInstance.publish(template.find('input').value, 
+        ArtFactory.publish(template.find('input').value, 
                                    {from: web3.eth.accounts[0], gas: 500000}
         );
         var ipfs = window.IpfsApi();
