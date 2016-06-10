ea57b895ad65881fea3980f19ddaf298db801653
Bill Gleim <bgleim@gmail.com>
2016-06-09 20:20:41 -0700

    Initial two-tier paywall contract structure

--- ArtFactory/mist/ArtFactory.sol
+++ ArtFactory/mist/ArtFactory.sol
@@ -2,76 +2,82 @@ contract ArtFactory {
   // internal variables
   address factoryOwner;
   
-  // one piece of art per address
-  mapping (address => uint) publicationPrice;
-
-  // any piece of art can be purchased by anyone
-  struct Purchases { mapping(address => uint) paid; }
-  
-  // track who has paid and how much per piece of art
-  mapping (address => Purchases) amountsPaid;
-  
   // log the Publish events
   event Publish(address art, uint price);
-
-  // log the Deposit events
-  event Deposit(address to, address from, uint amount);
-
-  // log the Deposit events
-  event PortalPaid(address to, address from, uint amount);
-
-  // log the Contribution to Factory events
-  event FactoryContribution(address from, uint amount);
 
   function ArtFactory() {
     factoryOwner = msg.sender;
   }
 
-  function publish() {
-    publicationPrice[msg.sender] = msg.value;
-	Publish(msg.sender, msg.value);
+  function () {
+    new Art(msg.sender, msg.value);
+  }
+}
+
+contract Art {
+  address artist;
+  uint price;
+  uint balance;
+  mapping (address => uint) amountPaid;
+  mapping (address => bool) purchased;
+
+  function Art(address _artist, uint _price) {
+    artist = _artist;
+    price = _price;
+    Publish(msg.sender, msg.value);
   }
 
-  // portal-facilitated transaction: portal receives part of payment
-  function pay(address recipient, address portal) {
-    if (msg.value > 0) {
-    	uint portalPayAmount = msg.value/100;
+  // default payment mechanism, triggers view rights upon reaching price threshold
+  function () {
+    // add to value associated with payer
+    amountPaid[msg.sender] += msg.value;
 
-    	recipient.send(msg.value - portalPayAmount);
-
-    	// one percent to referring portal
-    	portal.send(portalPayAmount);
-
-    	// send notification portal has been paid
-    	PortalPaid(portal, msg.sender, portalPayAmount);
-
-        // extract purchases for the artist
-    	Purchases purchases = amountsPaid[recipient];
-    	
-    	// add purchase to artist purchase record
-    	purchases.paid[msg.sender] += msg.value;
-
-    	// send notification deposit is complete
-        Deposit(recipient, msg.sender, msg.value);
+    // if reached purchase threshold, set state to purchased
+    if (amountPaid[msg.sender] >= _price) {
+      purchased[msg.sender] = true;
     }
+
+    // add to the contract balance
+    balance += msg.value;
+
+    // notify watchers of Deposit event
+    Deposit(msg.sender, msg.value);
   }
 
-  // check if sender has paid
-  function view(address art) constant returns (bool result) {
-    // extract purchases for the artist
-	Purchases purchases = amountsPaid[art];
-	
-	if (purchases.paid[msg.sender] >= publicationPrice[art]) {
-	    return true;
-	}
-	return false;
-      
+  // let caller know if they possess viewing rights
+  function view() constant returns(bool) {
+    // let caller know if we reached purchase threshold
+    return purchased[msg.sender];
   }
 
-  // simplest transaction: someone contributes to factory
-  function() {
-    if (msg.value > 0) {
-      FactoryContribution(msg.sender, msg.value);
+  // return balance
+  function viewBalance() constant returns(uint) {
+    if (msg.sender != artist) throw;
+
+    return balance;
+  }
+
+  // withdraw complete balance
+  function withdraw() returns(uint) {
+    if (msg.sender != artist) throw;
+
+    // do not handle the case if send() has been overridden and fails
+    if (msg.sender.send(balance)) {
+      balance = 0;
     }
+    return balance;
+  }
+
+  // withdraw up to complete balance
+  function withdraw(uint value) returns(uint) {
+    if (msg.sender != artist) throw;
+
+    if (value <= balance) {
+      // do not handle the case if send() has been overridden and fails
+      if (msg.sender.send(value)) {
+        balance -= value;
+      }
+    }
+    return balance;
   }
 }
